c82bcc4db16fb5dcc8c312a8c93fa7387c1192f4
baselkorj <baselanwarkorj@gmail.com>
2021-06-06 17:35:51 +0400

    removed decimal module

--- pymetric.py
+++ pymetric.py
@@ -1,111 +1,108 @@
-from decimal import Decimal
-
-
 def atto(x):
     temp = x * 1000000000000000000
-    x = str(Decimal(temp)) + "a"
+    x = str(temp) + "a"
     return x
 
 
 def femto(x):
     temp = x * 1000000000000000
-    x = str(Decimal(temp)) + "f"
+    x = str(temp) + "f"
     return x
 
 
 def pico(x):
     temp = x * 1000000000000
-    x = str(Decimal(temp)) + "p"
+    x = str(temp) + "p"
     return x
 
 
 def nano(x):
     temp = x * 1000000000
-    x = str(Decimal(temp)) + "n"
+    x = str(temp) + "n"
     return x
 
 
 def micro(x):
     temp = x * 1000000
-    x = str(Decimal(temp)) + "μ"
+    x = str(temp) + "μ"
     return x
 
 
 def mili(x):
     temp = x * 1000
-    x = str(Decimal(temp)) + "m"
+    x = str(temp) + "m"
     return x
 
 
 def centi(x):
     temp = x * 100
-    x = str(Decimal(temp)) + "c"
+    x = str(temp) + "c"
     return x
 
 
 def deci(x):
     temp = x * 10
-    x = str(Decimal(temp)) + "d"
+    x = str(temp) + "d"
     return x
 
 
 def deka(x):
     temp = x / 10
-    x = str(Decimal(temp)) + "da"
+    x = str(temp) + "da"
     return x
 
 
 def hecto(x):
     temp = x / 100
-    x = str(Decimal(temp)) + "h"
+    x = str(temp) + "h"
     return x
 
 
 def kilo(x):
     temp = x / 1000
-    x = str(Decimal(temp)) + "k"
+    x = str(temp) + "k"
     return x
 
 
 def mega(x):
     temp = x / 1000000
-    x = str(Decimal(temp)) + "M"
+    x = str(temp) + "M"
     return x
 
 
 def giga(x):
-    temp = x * 1000000000
-    x = str(Decimal(temp)) + "G"
+    temp = x / 1000000000
+    x = str(temp) + "G"
     return x
 
 
 def tera(x):
-    temp = x * 1000000000000
-    x = str(Decimal(temp)) + "T"
+    temp = x / 1000000000000
+    x = str(temp) + "T"
     return x
 
 
 def peta(x):
-    temp = x * 1000000000000000
-    x = str(Decimal(temp)) + "P"
+    temp = x / 1000000000000000
+    x = str(temp) + "P"
     return x
 
 
 def exa(x):
-    temp = x * 1000000000000000000
-    x = str(Decimal(temp)) + "E"
+    temp = x / 1000000000000000000
+    x = str(temp) + "E"
     return x
 
 
 def zetta(x):
-    temp = x * 1000000000000000000000
-    x = str(Decimal(temp)) + "Z"
+    temp = x / 1000000000000000000000
+    x = str(temp) + "Z"
     return x
 
 
 def yotta(x):
-    temp = x * 1000000000000000000000000
-    x = str(Decimal(temp)) + "Y"
+    temp = x / 1000000000000000000000000
+    x = str(temp) + "Y"
     return x
 
 
