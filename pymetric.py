2d7ac6ef72b43e0c92e3fb63b6e2dd6a1b226399
Abdulrhman Ghnem <abdalrhman.ib1998@gmail.com>
2021-07-08 18:26:08 +0800

    added the round_Num function

--- pymetric.py
+++ pymetric.py
@@ -3,87 +3,87 @@ hi = ["", "k", "M", "G", "T", "P", "E", "Z", "Y"]
 low = ["", "m", "μ", "p", "f", "a"]
 
 
-def atto(x):
+def atto(x, y=0):
     temp = x * 1000000000000000000
-    x = str(temp) + "a"
+    x = str(round_Num(temp, y)) + "a"
     return x
 
 
-def femto(x):
+def femto(x, y=0):
     temp = x * 1000000000000000
-    x = str(temp) + "f"
+    x = str(round_Num(temp, y)) + "f"
     return x
 
 
-def pico(x):
+def pico(x, y=0):
     temp = x * 1000000000000
-    x = str(temp) + "p"
+    x = str(round_Num(temp, y)) + "p"
     return x
 
 
-def nano(x):
+def nano(x, y=0):
     temp = x * 1000000000
-    x = str(temp) + "n"
+    x = str(round_Num(temp, y)) + "n"
     return x
 
 
-def micro(x):
+def micro(x, y=0):
     temp = x * 1000000
-    x = str(temp) + "μ"
+    x = str(round_Num(temp, y)) + "μ"
     return x
 
 
-def mili(x):
+def milli(x, y=0):
     temp = x * 1000
-    x = str(temp) + "m"
+    x = str(round_Num(temp, y)) + "m"
     return x
 
 
-def kilo(x):
+def kilo(x, y=0):
     temp = x / 1000
-    x = str(temp) + "k"
+    x = str(round_Num(temp, y)) + "k"
     return x
 
 
-def mega(x):
+def mega(x, y=0):
     temp = x / 1000000
-    x = str(temp) + "M"
+    x = str(round_Num(temp, y)) + "M"
     return x
 
 
-def giga(x):
+def giga(x, y=0):
     temp = x / 1000000000
-    x = str(temp) + "G"
+    x = str(round_Num(temp, y)) + "G"
     return x
 
 
-def tera(x):
+def tera(x, y=0):
     temp = x / 1000000000000
-    x = str(temp) + "T"
+    x = str(round_Num(temp, y)) + "T"
     return x
 
 
-def peta(x):
+def peta(x, y=0):
     temp = x / 1000000000000000
-    x = str(temp) + "P"
+    x = str(round_Num(temp, y)) + "P"
     return x
 
 
-def exa(x):
+def exa(x, y=0):
     temp = x / 1000000000000000000
-    x = str(temp) + "E"
+    x = str(round_Num(temp, y)) + "E"
     return x
 
 
-def zetta(x):
+def zetta(x, y=0):
     temp = x / 1000000000000000000000
-    x = str(temp) + "Z"
+    x = str(round_Num(temp, y)) + "Z"
     return x
 
 
-def yotta(x):
+def yotta(x, y=0):
     temp = x / 1000000000000000000000000
-    x = str(temp) + "Y"
+    x = str(round_Num(temp, y)) + "Y"
     return x
 
 
@@ -131,3 +131,14 @@ def help():  # this is not needed
 
 def lisence():  # this is not needed
     print("This is the lisence.")  # this is not needed
+
+
+def round_Num(
+    x, y
+):  # this function to see if the user wants to round the results or not
+    if y == 0:
+        th = str(x)
+    else:
+        th = str(round(x, y))
+
+    return th
---
+++ test.py
@@ -0,0 +1,5 @@
+import pymetric
+
+y = pymetric.kilo(0.56893452, 4)
+
+print(y)
