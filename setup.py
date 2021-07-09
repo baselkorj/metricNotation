19415b0b0fb6481406c2c9762fe58f951cc82061
baselkorj <baselanwarkorj@gmail.com>
2021-07-09 10:45:29 -0400

    create setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name="pymetro",
+    packages=["pymetro"],
+    version="0.1",
+    license="GPL-3.0",
+    description="A library for formatting numbers according to the metric system ",
+    author=["Abdulrhman-AIG", "baselkorj"],
+    author_email=["abdalrhman.ib1998@gmail.com", "baselanwarkorj@gmail.com"],
+    url="https://github.com/baselkorj/pymetro",
+    download_url="https://github.com/user/pymetro/archive/pymetro_v01.tar.gz",
+    keywords=["Units", "Metric", "Scientific Notation", "SI"],
+    classifiers=[
+        "Development Status :: 1 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
